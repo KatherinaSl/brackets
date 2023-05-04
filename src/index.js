7cfc7990d48c160693dc0f0c92b242415ea25192
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2023-05-04 15:38:09 +0600

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = []
+
+  for(i=0; i<str.length; i++) {
+    if(str.charAt(i) === arr[arr.length - 1]  && isSpecial(str.charAt(i), bracketsConfig)) {
+      let openBracket = arr.pop()
+      if(!openBracket || !isPair(openBracket, str.charAt(i), bracketsConfig)){
+        return false
+      }
+    } else if (isOpeningBracket(str.charAt(i), bracketsConfig)) {
+      arr.push(str.charAt(i))
+    } else if(!isOpeningBracket(str.charAt(i), bracketsConfig) ) {
+      let openBracket = arr.pop()
+      if(!openBracket || !isPair(openBracket, str.charAt(i), bracketsConfig)){
+        return false
+      }
+    }
+  }
+  return arr.length === 0
+}
+
+
+
+function isOpeningBracket(element, bracketsConfig) {
+  for(item of bracketsConfig) {
+    if(element === item[0]) {
+      return true
+    } 
+  }
+  return false
+}
+
+function isPair(openBracket, closeBracket, bracketsConfig) {
+  for(item of bracketsConfig) {
+    if(openBracket === item[0] && closeBracket === item[1]) {
+      return true
+    } 
+  }
+  return false
+} 
+
+function isSpecial(element, bracketsConfig) {
+  return isPair(element, element, bracketsConfig)
 }
